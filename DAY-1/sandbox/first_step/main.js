9272b909c41114f5d11f26fc1ba5b7acae1593b3
Dechogt <dechardinlepro@gmail.com>
2025-04-14 21:04:16 +0200

    Dernier push pour ce premier jour: Ajout des autres fonctions de calcul (différence, produit et quotient)

--- DAY-1/sandbox/first_step/main.js
+++ DAY-1/sandbox/first_step/main.js
@@ -1,3 +1,7 @@
-import { add } from './calcul.js'
+import { add, diff, prod, quot } from './calcul.js'
 
-console.log(add(10, 3)) // Affiche 13
+console.log("Prenons a = 10 et b = 3 pour tester ces fonctions") 
+console.log("En faisant l'addition de a et b on obtient " + add(10, 3)) // Affiche 13
+console.log("En faisant la différence de a par b on obtient " + diff(10, 3)) // Affiche 7
+console.log("En faisant le produit de a et b on obtient " + prod(10, 3)) // Affiche 30
+console.log("En faisant le quotient de a et b on obtient " + quot(10, 3)) // Affiche 3.33
