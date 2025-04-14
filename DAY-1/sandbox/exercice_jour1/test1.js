48b1297273a826dd3395d7c26440ebb492b6c881
Dechogt <dechardinlepro@gmail.com>
2025-04-14 12:00:46 +0200

    Premier push via wsl, initialisation du projet avec sa structure globale

---
+++ DAY-1/sandbox/exercice_jour1/test1.js
@@ -0,0 +1,24 @@
+// let est utilisé pour les variables qui peuvent changer de valeur
+let nom= "Decho"
+
+// var n'est plus utilisé de nos jours, on utilise let ou const
+var prenom= "Jean"
+// const est utilisé pour les variables qui ne changent pas de valeur
+const age= 25
+const ville= "Paris"
+const arr= [
+    1, 2, 3, 4, 5]
+const objet= 
+    {
+        nom: "Decho", 
+        prenom: "Jean", 
+        age: 25, 
+        ville: "Paris"
+}
+// console.log est utilisé pour afficher des messages dans la console
+console.log(nom)
+console.log(prenom)
+console.log(age)
+console.log(ville)
+console.log(arr)
+
