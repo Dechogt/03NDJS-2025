c11f8889e0e49f37d0001e59fac955a73442c6e7
Dechogt <dechardinlepro@gmail.com>
2025-04-14 13:18:15 +0200

    Evolution dans notre apprentissage de Node Js

--- DAY-1/sandbox/exercice_jour1/test1.js
+++ DAY-1/sandbox/exercice_jour1/test1.js
@@ -22,3 +22,49 @@ console.log(age)
 console.log(ville)
 console.log(arr)
 
+// les loupes sont utilisées pour répéter une action plusieurs fois
+//exemple avec for
+for (let i=0; i<5; i++){
+    console.log(i)
+}
+// exemple avec while
+let i=0
+while (i<5){
+    console.log(i)
+    i++
+}
+// exemple avec do while
+let j=0
+do {
+    console.log(j)
+    j++
+}
+while (j<5)
+// exemple avec forEach
+arr.forEach(function(element){
+    console.log(element)
+})
+// exemple avec map
+const arr2= arr.map(function(element){
+    return element*2
+})
+console.log(arr2)
+
+
+// Export/Import via ES6 en supposant que cette fonction dans un fichier math.js
+export const somme= (a, b) => {
+    return a+b
+}
+
+import {somme} from './math.js'
+console.log(somme(2, 3)) //5 
+
+// Export/Import directement comme un module node js
+function somme(a, b) {
+    return a + b;
+}
+
+module.exports = somme;
+const somme = require('./math');
+
+console.log(somme(2, 3)); // Affiche 5
